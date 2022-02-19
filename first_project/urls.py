5f9b685fda649fc68e026349543d69b24de95f7a
Iusar <sarkisov.yy@gmail.com>
2022-02-19 18:03:02 +0300

    inicial

---
+++ first_project/urls.py
@@ -0,0 +1,12 @@
+from app.views import home_view, time_view, workdir_view
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('', home_view, name='home'),
+    # Раскомментируйте код, чтобы данные урлы 
+    # обрабатывались Django
+    path('current_time/', time_view, name='time'),
+    path('workdir/', workdir_view, name='workdir'),
+    path('admin/', admin.site.urls),
+]
